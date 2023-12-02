799a654440318d0884d7efdb3be14faa3d83fadb
Adam-192 <adam.basa@gmail.com>
2023-12-02 17:20:33 +0100

    prva stram

--- Bookies_App/obj/Debug/net8.0-windows/Bookies_App.AssemblyInfo.cs
+++ Bookies_App/obj/Debug/net8.0-windows/Bookies_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookies_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5c5ea44b461fa589d89db6738e1da1826ccf37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookies_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookies_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
