a1fd1236286944ad2afd177c428535fed865b1f7
loginName1 <yan.benko@gmail.com>
2023-12-15 16:07:45 +0100

    created and documented API call (books)

--- Bookies_App/Utilities/API_URIs.cs
+++ Bookies_App/Utilities/API_URIs.cs
@@ -8,7 +8,7 @@ namespace Bookies_App.Utilities
 {
     class API_URIs
     {
-        public const string baseURI = "mongodb+srv://bookies:bookies@inkspire.lwgobmf.mongodb.net/inkspiredb?retryWrites=true&w=majority";
+        public const string baseURI = "http://localhost:5000/api/";
         public const string books = "books";
         public const string users = "users";
     }
